0ae5a1edf9840a6f50c591b9fdef226f48c9085d
David Adebayo Olude <oludedav@exflqr35160.desy.de>
2025-11-12 10:35:12 +0100

    I fixed an import error in the DoubleLineEdit component with regards to FONT FAMILY DEFAULT

--- src/components/controllers/editable/DoubleLineEdit.tsx
+++ src/components/controllers/editable/DoubleLineEdit.tsx
@@ -4,7 +4,7 @@ import DeviceOfflineOverlay from "@/components/DeviceOfflineOverlay";
 import { useKaraboPropertyInfo } from "@/components/shared/hooks/useKaraboProperty";
 import { useDeviceOnlineStatus } from "@/components/shared/hooks/useDeviceOnlineStatus";
 import { useKaraboKeysString } from "@/components/shared/hooks/useKaraboKeysString";
-import { FONT_FAMILY_DEFAULT } from "@/components/shared/helpers/QtFontDescriptor";
+import { FONT_FAMILY_DEFAULT } from "@/components/shared/helpers/fontDefaults";
 
 /**
  * DoubleLineEdit - Float input field with configurable decimal precision.
