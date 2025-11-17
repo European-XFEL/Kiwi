d85e123926f48b0e9596c9e02c45426d623fd476
Raul Costa <raul.costa@xfel.eu>
2025-11-17 11:44:35 +0100

    Decode rowSchema attribute of a table property

--- src/karabo_data/DeviceSchemaInfo.ts
+++ src/karabo_data/DeviceSchemaInfo.ts
@@ -13,21 +13,27 @@ export interface DeviceSchemaInfo {
   propertyDescriptors: Map<string, PropertySchemaAttributes>;
 }
 
+export interface TableColumnInfo {
+  columnName: string;
+  columnAttributes: PropertySchemaAttributes;
+}
+
 export interface PropertySchemaAttributes {
   valueType: HashTypes;
-  defaultValue: HashValueType;
+  defaultValue?: HashValueType;
   // displayType serves as a "guide" for the GUI client for displaying the
   // property. Usual values for it: "Slot", "ImageData", "NDArray", "OutputSchema"
   displayType?: string;
-  displayedName: string;
+  displayedName?: string;
   description?: string;
-  requiredAccessLevel: AccessLevel;
-  accessMode: AccessMode;
+  requiredAccessLevel?: AccessLevel;
+  accessMode?: AccessMode;
   archivePolicy?: ArchivePolicy;
   assignment?: Assignment;
   unitSymbol?: string;
   metricPrefixSymbol?: string;
-  nodeType: NodeType;
+  nodeType?: NodeType;
   options?: VectorElementType[];
   allowedStates?: string[];
+  rowSchema?: TableColumnInfo[];
 }
