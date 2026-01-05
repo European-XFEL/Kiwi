8ea3fc7a7c066775259faa95e90ae5082a9b00e4
Dennis Goeries <dennis.goeries@xfel.eu>
2026-01-05 17:11:14 +0100

    refactor: Add a Karabo Hash

---
+++ src/karabo-hash/__test__/serializer_bin.test.ts
@@ -0,0 +1,216 @@
+import fs from 'fs';
+import { Hash, HashList, Schema } from '@/karabo-hash/hash';
+import { decodeBinary, decodeBinarySchema } from '@/karabo-hash/bin_reader';
+import { encodeBinary } from '@/karabo-hash/bin_writer';
+
+import path from 'path';
+
+describe('binary', () => {
+  it('reading', () => {
+    const filePath = path.join(__dirname, 'conf_hash.bin');
+    const data = fs.readFileSync(filePath);
+    const hsh = decodeBinary(data);
+
+    // 1. Direct Access via get()
+    expect(hsh.getValue('deviceId')).toBe('Bob');
+    expect(hsh.getValue('classId')).toBe('PropertyTest');
+    expect(hsh.getValue('alarmCondition')).toBe('none');
+    expect(hsh.getValue('boolPropertyReadOnly')).toBe(false);
+    expect(hsh.getValue('uint16PropertyReadOnly')).toBe(32000);
+    expect(hsh.getValue('int16PropertyReadOnly')).toBe(3200);
+    expect(hsh.getValue('uint32PropertyReadOnly')).toBe(32000000);
+    expect(hsh.getValue('floatPropertyReadOnly')).toBe(3.1415960788726807);
+    expect(hsh.getValue('uint8PropertyReadOnly')).toBe(177);
+
+    expect(hsh.getValue('deviceId')).toBe('Bob');
+    expect(hsh.getValue('classId')).toBe('PropertyTest');
+    expect(hsh.getValue('alarmCondition')).toBe('none');
+    expect(hsh.getValue('boolPropertyReadOnly')).toBe(false);
+    expect(hsh.getValue('uint16PropertyReadOnly')).toBe(32000);
+    expect(hsh.getValue('int16PropertyReadOnly')).toBe(3200);
+    expect(hsh.getValue('uint32PropertyReadOnly')).toBe(32000000);
+    expect(hsh.getValue('floatPropertyReadOnly')).toBe(3.1415960788726807);
+    expect(hsh.getValue('uint8PropertyReadOnly')).toBe(177);
+
+    // 3. Test Attributes
+    // We cast to 'any' to access the raw .value_ property of the KaraboType wrapper
+    expect(
+      (hsh.getAttribute('uint8PropertyReadOnly', 'sec') as any).value_
+    ).toBe(1631725047n);
+    expect(
+      (hsh.getAttribute('uint8PropertyReadOnly', 'frac') as any).value_
+    ).toBe(661721908000000000n);
+    expect(
+      (hsh.getAttribute('uint8PropertyReadOnly', 'tid') as any).value_
+    ).toBe(0n);
+    expect(
+      (hsh.getAttribute('uint8PropertyReadOnly', 'alarmCondition') as any)
+        .value_
+    ).toBe('none');
+
+    // 4. Test Vectors (arrays)
+    expect(hsh.getValue('vectors.boolProperty')).toEqual([
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+    ]);
+    expect(hsh.getValue('vectors.uint8Property')).toEqual([
+      41, 42, 43, 44, 45, 46,
+    ]);
+    expect(hsh.getValue('vectors.int16Property')).toEqual([
+      20041, 20042, 20043, 20044, 20045, 20046,
+    ]);
+    expect(hsh.getValue('vectors.uint16Property')).toEqual([
+      10041, 10042, 10043, 10044, 10045, 10046,
+    ]);
+    expect(hsh.getValue('vectors.uint32Property')).toEqual([
+      90000041, 90000042, 90000043, 90000044, 90000045, 90000046,
+    ]);
+    expect(hsh.getValue('vectors.int64Property')).toEqual([
+      20000000041n,
+      20000000042n,
+      20000000043n,
+      20000000044n,
+      20000000045n,
+      20000000046n,
+    ]);
+    expect(hsh.getValue('vectors.stringProperty')).toEqual([
+      '1111111',
+      '2222222',
+      '3333333',
+      '4444444',
+      '5555555',
+      '6666666',
+    ]);
+
+    expect(hsh).toBeInstanceOf(Hash);
+
+    // 5. Iteration test
+    for (const [key, value, attrs] of hsh.iterallValues()) {
+      if (key === 'uint32PropertyReadOnly') {
+        expect(typeof value).not.toBe('object'); // Should be primitive number
+        expect(value).toBe(32000000);
+        expect(attrs['alarmCondition']).toBe('none');
+      }
+      if (key === 'output') {
+        expect(value).toBeInstanceOf(Hash);
+        // Check keys of the sub-hash
+        const keys = Array.from((value as Hash).keys());
+        expect(keys).toEqual(
+          expect.arrayContaining([
+            'bytesRead',
+            'bytesWritten',
+            'connections',
+            'distributionMode',
+            'hostname',
+            'noInputShared',
+            'port',
+            'schema',
+            'updatePeriod',
+          ])
+        );
+      }
+    }
+  });
+
+  it('writing', () => {
+    const filePath = path.join(__dirname, 'conf_hash.bin');
+
+    const data = fs.readFileSync(filePath);
+    const hsh = decodeBinary(data);
+
+    const new_data = encodeBinary(hsh);
+
+    const new_hsh = decodeBinary(new Uint8Array(new_data));
+
+    const keys = [
+      '_serverId_',
+      'deviceId',
+      'classId',
+      'alarmCondition',
+      'boolPropertyReadOnly',
+      'uint16PropertyReadOnly',
+      'int16PropertyReadOnly',
+      'uint32PropertyReadOnly',
+      'floatPropertyReadOnly',
+      'uint8PropertyReadOnly',
+    ];
+
+    for (const key of keys) {
+      expect(hsh.getValue(key)).toBe(new_hsh.getValue(key));
+    }
+
+    const table = new_hsh.get('table');
+    expect(table instanceof HashList).toBe(true);
+
+    // HashList / VectorHash handling
+    const read_table = hsh.getValue('table');
+    const new_table = new_hsh.getValue('table');
+
+    // Should be Array of Hash objects (unwrapped from HashList)
+    expect(read_table).toBeInstanceOf(Array);
+    expect(new_table).toBeInstanceOf(Array);
+
+    const read_table_arr = read_table as Hash[];
+    const new_table_arr = new_table as Hash[];
+
+    expect(read_table_arr.length).toBe(new_table_arr.length);
+
+    for (let i = 0; i < read_table_arr.length; i++) {
+      const read = read_table_arr[i];
+      const new_ = new_table_arr[i];
+      expect(read).toBeInstanceOf(Hash);
+      expect(new_).toBeInstanceOf(Hash);
+
+      const read_keys = Array.from(read.keys());
+      const new_keys = Array.from(new_.keys());
+
+      expect(read_keys).toEqual(new_keys);
+
+      expect(read.getValue('e1')).toBe(new_.getValue('e1'));
+      expect(read.getValue('e2')).toBe(new_.getValue('e2'));
+      expect(read.getValue('e3')).toBe(new_.getValue('e3'));
+      expect(read.getValue('e4')).toBe(new_.getValue('e4'));
+      // Use 5 digits precision for floating point comparison ~ 0.00001
+      expect(read.getValue('e5')).toBeCloseTo(new_.getValue('e5') as number, 5);
+    }
+  });
+
+  it('schema', () => {
+    const filePath = path.join(__dirname, 'schema_hash.bin');
+    const data = fs.readFileSync(filePath);
+    const schema = decodeBinarySchema(data);
+    expect(schema instanceof Schema).toBe(true);
+
+    expect(schema.name).toBe('PropertyTest');
+
+    for (const [key, value, attrs] of schema.hash.iterallValues()) {
+      if (key === 'uint32PropertyReadOnly') {
+        expect(value).toBe(0);
+        expect(attrs['nodeType']).toBe(0);
+        expect(attrs['valueType']).toBe('UINT32');
+        expect(attrs['displayedName']).toBe('UInt32 property read-only');
+      }
+      if (key === 'output') {
+        expect(value).toBeInstanceOf(Hash);
+        const keys = Array.from((value as Hash).keys());
+        expect(keys).toEqual(
+          expect.arrayContaining([
+            'bytesRead',
+            'bytesWritten',
+            'connections',
+            'distributionMode',
+            'hostname',
+            'noInputShared',
+            'port',
+            'schema',
+            'updatePeriod',
+          ])
+        );
+      }
+    }
+  });
+});
---
+++ src/karabo-hash/bin_reader.ts
@@ -0,0 +1,277 @@
+import * as Types from './types';
+import { Hash, Schema, HashList, HashAttributes } from './hash';
+import { TextDecoder } from 'util';
+
+type ParserFunction = (
+  parser: BinaryDecoder
+) => Types.KaraboValue | Hash | HashList | Schema;
+
+function readUInt8(parser: BinaryDecoder): Types.UInt8Value {
+  parser.pos += 1;
+  return new Types.UInt8Value(parser.dataview.getUint8(parser.pos - 1));
+}
+
+function readInt8(parser: BinaryDecoder): Types.Int8Value {
+  parser.pos += 1;
+  return new Types.Int8Value(parser.dataview.getInt8(parser.pos - 1));
+}
+
+function readUInt16(parser: BinaryDecoder): Types.UInt16Value {
+  parser.pos += 2;
+  return new Types.UInt16Value(parser.dataview.getUint16(parser.pos - 2, true));
+}
+
+function readInt16(parser: BinaryDecoder): Types.Int16Value {
+  parser.pos += 2;
+  return new Types.Int16Value(parser.dataview.getInt16(parser.pos - 2, true));
+}
+
+function readUInt32(parser: BinaryDecoder): Types.UInt32Value {
+  parser.pos += 4;
+  const value = parser.dataview.getUint32(parser.pos - 4, true);
+  return new Types.UInt32Value(value);
+}
+
+function readInt32(parser: BinaryDecoder): Types.Int32Value {
+  parser.pos += 4;
+  return new Types.Int32Value(parser.dataview.getInt32(parser.pos - 4, true));
+}
+
+function readUInt64(parser: BinaryDecoder): Types.UInt64Value {
+  parser.pos += 8;
+  const value = parser.dataview.getBigUint64(parser.pos - 8, true);
+  return new Types.UInt64Value(value);
+}
+
+function readInt64(parser: BinaryDecoder): Types.Int64Value {
+  parser.pos += 8;
+  const value = parser.dataview.getBigInt64(parser.pos - 8, true);
+  return new Types.Int64Value(value);
+}
+
+function readFloat32(parser: BinaryDecoder): Types.Float32Value {
+  parser.pos += 4;
+  return new Types.Float32Value(
+    parser.dataview.getFloat32(parser.pos - 4, true)
+  );
+}
+
+function readFloat64(parser: BinaryDecoder): Types.Float64Value {
+  parser.pos += 8;
+  return new Types.Float64Value(
+    parser.dataview.getFloat64(parser.pos - 8, true)
+  );
+}
+
+function readBool(parser: BinaryDecoder): Types.BoolValue {
+  parser.pos += 1;
+  return new Types.BoolValue(parser.data[parser.pos - 1] === 1);
+}
+
+function readVectorBool(parser: BinaryDecoder): Types.VectorBoolValue {
+  const size = readUInt32(parser).value_;
+  const start = parser.pos;
+  parser.pos = start + size;
+  const arr = Array.from(parser.data.slice(start, parser.pos)).map(
+    (m) => m > 0
+  );
+  return new Types.VectorBoolValue(arr);
+}
+
+function readChar(parser: BinaryDecoder): Types.CharValue {
+  parser.pos += 1;
+  return new Types.CharValue(parser.data[parser.pos - 1]);
+}
+
+function readVectorChar(parser: BinaryDecoder): Types.VectorCharValue {
+  const size = readUInt32(parser).value_;
+  parser.pos += size;
+  return new Types.VectorCharValue(
+    parser.data.slice(parser.pos - size, parser.pos)
+  );
+}
+
+function readString(parser: BinaryDecoder): Types.StringValue {
+  const size = readUInt32(parser).value_;
+  const content = parser.data.slice(parser.pos, parser.pos + size);
+  const str = parser.string_encoder.decode(content);
+  parser.pos += size;
+  return new Types.StringValue(str);
+}
+
+function readVectorString(parser: BinaryDecoder): Types.VectorStringValue {
+  let size = readUInt32(parser).value_;
+  const res = new Types.VectorStringValue([]);
+  while (size > 0) {
+    const element = readString(parser);
+    res.value_.push(element.value_);
+    size -= 1;
+  }
+  return res;
+}
+
+function buildVectorReader(elementReader: any, klass: any) {
+  return (parser: BinaryDecoder) => {
+    let size = readUInt32(parser).value_;
+    const slice_ = [];
+    while (size > 0) {
+      const element = elementReader(parser);
+      slice_.push(element.value_);
+      size -= 1;
+    }
+    return new klass(slice_);
+  };
+}
+
+function readSchema(parser: BinaryDecoder): Schema {
+  const l = readUInt32(parser).value_;
+  const op = parser.pos;
+  const nameSize = parser.data[parser.pos];
+  parser.pos++;
+  const content = parser.data.slice(parser.pos, parser.pos + nameSize);
+  const name = parser.string_encoder.decode(content);
+  parser.pos += nameSize;
+  const hsh = parser.readHash();
+  if (parser.pos - op !== l) {
+    throw new Error('Parser failed parsing Schema');
+  }
+  // Construct new Schema
+  return new Schema(name, hsh);
+}
+
+function parserUndefined(parser: BinaryDecoder, type: number): any {
+  throw new Error(`Parser not Implemented ${type}`);
+}
+
+const parsers = [
+  readBool, // Bool = 0
+  readVectorBool, // VectorBool = 1
+  readChar, // Char = 2
+  readVectorChar, // VectorChar = 3
+  readInt8, // Int8 = 4
+  buildVectorReader(readInt8, Types.VectorInt8Value), //  VectorInt8 = 5
+  readUInt8, // UInt8 = 6
+  buildVectorReader(readUInt8, Types.VectorUInt8Value), // VectorUInt8 = 7
+  readInt16, // Int16 = 8
+  buildVectorReader(readInt16, Types.VectorInt16Value), // VectorInt16 = 9
+  readUInt16, // UInt16 = 10
+  buildVectorReader(readUInt16, Types.VectorUInt16Value), // VectorUInt16 = 11
+  readInt32, // Int32 = 12
+  buildVectorReader(readInt32, Types.VectorInt32Value), // VectorInt32 = 13
+  readUInt32, // UInt32 = 14
+  buildVectorReader(readUInt32, Types.VectorUInt32Value), // VectorUInt32 = 15
+  readInt64, // Int64 = 16
+  buildVectorReader(readInt64, Types.VectorInt64Value), // VectorInt64 = 17
+  readUInt64, // UInt64 = 18
+  buildVectorReader(readUInt64, Types.VectorUInt64Value), // VectorUInt64 = 19
+  readFloat32, // Float = 20
+  buildVectorReader(readFloat32, Types.VectorFloat32Value), // VectorFloat = 21
+  readFloat64, // Double = 22
+  buildVectorReader(readFloat64, Types.VectorFloat64Value), // VectorDouble = 23
+  (p: BinaryDecoder) => parserUndefined(p, 24), // ComplexFloat = 24
+  (p: BinaryDecoder) => parserUndefined(p, 25), // VectorComplexFloat = 25
+  (p: BinaryDecoder) => parserUndefined(p, 26), // ComplexDouble = 26
+  (p: BinaryDecoder) => parserUndefined(p, 27), // VectorComplexDouble = 27
+  readString, // String = 28
+  readVectorString, // VectorString = 29
+  (p: BinaryDecoder) => p.readHash(), // Hash = 30
+  (p: BinaryDecoder) => p.readVectorHash(), // VectorHash = 31
+  readSchema, // Schema = 32
+  (p: BinaryDecoder) => parserUndefined(p, 33), // missing 33
+  (p: BinaryDecoder) => parserUndefined(p, 34), // missing 34
+  (p: BinaryDecoder) => parserUndefined(p, 35), // None_ = 35
+  (p: BinaryDecoder) => parserUndefined(p, 36), // missing 36
+  readVectorChar, // ByteArray = 37
+];
+
+function getParser(typeNumber: number): ParserFunction {
+  const parser = parsers[typeNumber];
+  if (typeof parser != 'function') {
+    throw new Error('failed to find parser for type ' + typeNumber);
+  }
+  return parser;
+}
+
+class BinaryDecoder {
+  dataview: DataView;
+
+  pos = 0;
+
+  string_encoder = new TextDecoder('utf-8');
+
+  key_decoder = new TextDecoder('ascii');
+
+  constructor(public data: Uint8Array) {
+    this.dataview = new DataView(this.data.buffer);
+  }
+
+  readKey(): string {
+    const size = this.data[this.pos];
+    const start = this.pos + 1;
+    this.pos = start + size;
+    return this.key_decoder.decode(this.data.slice(start, this.pos));
+  }
+
+  read(): Hash {
+    return this.readHash();
+  }
+
+  readVectorHash(): HashList {
+    let size = readUInt32(this).value_;
+    const ret: Hash[] = [];
+    while (size > 0) {
+      ret.push(this.readHash());
+      size -= 1;
+    }
+    return new HashList(ret);
+  }
+
+  readHash(): Hash {
+    let size = readUInt32(this).value_;
+    const hash = new Hash();
+
+    while (size > 0) {
+      const key = this.readKey();
+      const hashType = readUInt32(this).value_;
+
+      let asize = readUInt32(this).value_;
+
+      // Collect attributes into a Map for the new Hash
+      const attrs: HashAttributes = new Map();
+      while (asize > 0) {
+        const attrKey = this.readKey();
+        const attrType = readUInt32(this).value_;
+        const attrValue = getParser(attrType)(this);
+        attrs.set(attrKey, attrValue);
+        asize -= 1;
+      }
+
+      // Read the main value
+      const value = getParser(hashType)(this);
+
+      // Use setElement to properly populate the HashNode with data and attributes
+      hash.setElement(key, value, attrs);
+
+      size -= 1;
+    }
+    return hash;
+  }
+
+  readSchema(): Schema {
+    return readSchema(this);
+  }
+}
+
+function decodeBinary(bin_hash: Uint8Array): Hash {
+  const parser = new BinaryDecoder(bin_hash);
+  const data = parser.read();
+  return data;
+}
+
+function decodeBinarySchema(bin_hash: Uint8Array): Schema {
+  const parser = new BinaryDecoder(bin_hash);
+  const data = parser.readSchema();
+  return data;
+}
+
+export { decodeBinary, decodeBinarySchema };
