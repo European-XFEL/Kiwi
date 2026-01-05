b6101b1c93adcc1cbea485577ac64a6b066e7f12
Dennis Goeries <dennis.goeries@xfel.eu>
2026-01-05 09:16:13 +0100

    feat: Add reconfigure protocol

---
+++ src/karabo_hash/builders/command_reconfigure.ts
@@ -0,0 +1,15 @@
+import { Hash, makeHash } from 'karabo-ts';
+import { REQUEST_REPLY_TIMEOUT } from './const.js';
+
+export const buildReconfigureHash = (
+  deviceId: string,
+  configuration: Hash
+): Hash => {
+  return makeHash({
+    type: 'reconfigure',
+    deviceId: deviceId,
+    configuration: configuration,
+    reply: true,
+    timeout: REQUEST_REPLY_TIMEOUT,
+  });
+};
