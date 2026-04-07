35fe4b7b444b843084be1cd27fed3c3990fa6999
Dennis Goeries <dennis.goeries@xfel.eu>
2026-04-07 13:23:41 +0200

    feat: First implementation for call device slot

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,29 @@
+import type { Config } from 'jest';
+
 export default {
   testEnvironment: 'jest-environment-jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
   moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
-    // This mock might not be needed
     '^karabo-ts$': '<rootDir>/test/__mocks__/karabo-ts.ts',
-    // Keep this for actual Worker imports
     '\\?worker$': '<rootDir>/test/__mocks__/WorkerMock.ts',
     '^react-plotly.js$': '<rootDir>/test/__mocks__/plotlyMock.js',
+    '^uuid$': '<rootDir>/test/__mocks__/uuidMock.ts',
   },
+
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         tsconfig: '<rootDir>/tsconfig.app.json',
         babelConfig: true,
+        useESM: true,
       },
     ],
   },
-};
+} satisfies Config;
